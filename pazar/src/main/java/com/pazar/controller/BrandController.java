ce980455ab954da9e9cd2fe0ebf06d3aa4e17cea
Bulut <mervebulut.02@gmail.com>
2022-08-04 18:13:55 +0300

    Brand Create page created. And Brand Controller created.

--- pazar/src/main/java/com/pazar/controller/BrandController.java
+++ pazar/src/main/java/com/pazar/controller/BrandController.java
@@ -2,7 +2,10 @@ package com.pazar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.pazar.model.Brand;
 import com.pazar.service.BrandService;
 
 @Controller
@@ -10,10 +13,15 @@ public class BrandController {
 
 	@Autowired
 	private BrandService brandService;
-	
-	  public BrandController(BrandService brandService) {
-	        this.brandService = brandService;
-	    }
-	
-	
+
+	public BrandController(BrandService brandService) {
+		this.brandService = brandService;
+	}
+
+	@GetMapping("/savebrand")
+	public String saveBrand(@ModelAttribute("brandVM") Brand brand) {
+		return "brand/create";
+	}
+
+
 }
