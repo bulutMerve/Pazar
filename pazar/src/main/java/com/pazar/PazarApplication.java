64620c724e8d685b4d7a702f19af36c56aaa9bcb
Bulut <mervebulut.02@gmail.com>
2022-08-04 15:11:42 +0300

    first commit

---
+++ pazar/src/main/java/com/pazar/PazarApplication.java
@@ -0,0 +1,13 @@
+package com.pazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PazarApplication.class, args);
+	}
+
+}
