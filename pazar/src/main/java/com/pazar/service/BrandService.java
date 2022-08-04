d9d9485e0db735425ce8ce518d81ed3cb022cd60
Bulut <mervebulut.02@gmail.com>
2022-08-04 16:36:18 +0300

    Brand Service added.

---
+++ pazar/src/main/java/com/pazar/service/BrandService.java
@@ -0,0 +1,10 @@
+package com.pazar.service;
+
+import java.util.List;
+import com.pazar.model.Brand;
+
+public interface BrandService {
+	Brand SaveBrand (Brand brand);
+	List<Brand> listBrand();
+	void deleteBrand(int id);
+}
