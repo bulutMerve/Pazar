4658d10fc8d271f9adad7b85ba277872a2d586d0
Bulut <mervebulut.02@gmail.com>
2022-08-11 19:56:21 +0300

    add list.html edit companycontroller add savecompany method to companyconroller add companylist method to controller

--- pazar/src/main/java/com/pazar/controller/CompanyController.java
+++ pazar/src/main/java/com/pazar/controller/CompanyController.java
@@ -1,9 +1,14 @@
 package com.pazar.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pazar.model.Company;
 import com.pazar.service.CompanyService;
@@ -21,5 +26,20 @@ public class CompanyController {
 	@GetMapping("/savecompany")
 	public String createCompany(@ModelAttribute("companyVM") Company company) {
 		return "company/create";
+	}
+	
+	@PostMapping("/savecompany")
+	public String saveCompany(Company company) {
+		companyService.saveCompany(company);
+		return "redirect:/companylist";
+	}
+	
+	@RequestMapping("/companylist")
+	public String companyList(Model model) {
+		List <Company> companyList = companyService.listCompany();
+		model.addAttribute("companyListVM",companyList);
+		return "company/list";
+		
+		
 	}
 }
