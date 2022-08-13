4ec6bb6355aaa2695a21a5b1b29075e3836b8f53
Bulut <mervebulut.02@gmail.com>
2022-08-13 15:18:13 +0300

    add update.html edit company controller

--- pazar/src/main/java/com/pazar/controller/CompanyController.java
+++ pazar/src/main/java/com/pazar/controller/CompanyController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,31 +16,36 @@ import com.pazar.service.CompanyService;
 
 @Controller
 public class CompanyController {
-	
+
 	@Autowired
 	private CompanyService companyService;
 
 	public CompanyController(CompanyService companyService) {
-		this.companyService=companyService;
+		this.companyService = companyService;
 	}
-	
+
 	@GetMapping("/savecompany")
 	public String createCompany(@ModelAttribute("companyVM") Company company) {
 		return "company/create";
 	}
-	
+
 	@PostMapping("/savecompany")
 	public String saveCompany(Company company) {
 		companyService.saveCompany(company);
 		return "redirect:/companylist";
 	}
-	
+
 	@RequestMapping("/companylist")
 	public String companyList(Model model) {
-		List <Company> companyList = companyService.listCompany();
-		model.addAttribute("companyListVM",companyList);
+		List<Company> companyList = companyService.listCompany();
+		model.addAttribute("companyListVM", companyList);
 		return "company/list";
-		
-		
+	}
+	
+	@GetMapping ("/updatecompany/{id}")
+	public String updateCompany(@PathVariable("id")int id, Model model) {
+		Company company = companyService.getCompany(id);
+		model.addAttribute("companyVM",company);
+		return "company/update";
 	}
 }
