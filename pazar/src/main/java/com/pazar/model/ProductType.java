cf39260115b72912569c9f2e4af25cdf489efde9
Bulut <mervebulut.02@gmail.com>
2022-08-16 16:20:24 +0300

    create product type model

---
+++ pazar/src/main/java/com/pazar/model/ProductType.java
@@ -0,0 +1,29 @@
+package com.pazar.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class ProductType {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(nullable=false)
+	private int productTypeId;
+	
+	@Column(nullable=false)
+	private String productTypeName;
+	
+	
+	
+}
