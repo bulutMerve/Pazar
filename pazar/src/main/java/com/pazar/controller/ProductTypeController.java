3dae3c096b5a1015bdbab86c526d9f3a57c7b362
Bulut <mervebulut.02@gmail.com>
2022-08-30 18:20:48 +0300

    add list.html add producttypeList method to controller

--- pazar/src/main/java/com/pazar/controller/ProductTypeController.java
+++ pazar/src/main/java/com/pazar/controller/ProductTypeController.java
@@ -1,10 +1,14 @@
 package com.pazar.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pazar.model.Brand;
 import com.pazar.model.ProductType;
@@ -29,6 +33,13 @@ public class ProductTypeController {
 	    public String saveProductType(ProductType productType) {
 		  productTypeService.saveProductType(productType);
 	        return "redirect:/producttypelist";
+	    }
+	  
+	  @RequestMapping("/producttypelist")
+	    public String producttypeList(Model model) {
+	        List<ProductType> producttypeList = productTypeService.listProductType();
+	        model.addAttribute("producttypeListVM", producttypeList);
+	        return "producttype/list";
 	    }
 
 }
